ddbacb4f1056d2d8bc0d066c3bd2faca5b5381d7
kittiposete <96778671+kittiposete@users.noreply.github.com>
2025-08-07 10:02:07 +0700

    Add configuration files for Android project setup

--- app/src/main/java/com/borntodev/mathgame/MainActivity.kt
+++ app/src/main/java/com/borntodev/mathgame/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    private var btn_start_game_main:Button ?= null
+    private var btn_start_game_main: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
